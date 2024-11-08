4c8998586885b07a5efd370c9ed6f0a85263d54f
joyymungaii <joywanjirum4@gmail.com>
2024-11-08 11:21:51 +0300

    updatesssss

---
+++ src/adds.java
@@ -0,0 +1,16 @@
+interface Adding {
+    static int add(int a, int b) {
+        return a + b;
+    }
+}
+
+public class adds {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+
+        int sum = Adding.add(num1, num2);
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
